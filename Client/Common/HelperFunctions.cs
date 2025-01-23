999f0d8182083eb64d0a546ffbdfb8c17b93f947
Dinesh Jethoe <dinesh.jethoe@live.com>
2025-01-23 19:28:59 +0100

    add placement zone option in form

---
+++ Client/Common/HelperFunctions.cs
@@ -0,0 +1,21 @@
+﻿using MudBlazor;
+
+namespace Client.Common
+{
+    internal static class HelperFunctions
+    {
+        internal static string GetPlacementZoneIcon(string loc)
+        {
+            return loc switch
+            {
+                "Top" => @Icons.Material.Filled.VerticalAlignTop,
+                "Bottom" => @Icons.Material.Filled.VerticalAlignBottom,
+                "Middle" => @Icons.Material.Filled.VerticalAlignCenter,
+                "Right" => @Icons.Material.Filled.AlignHorizontalRight,
+                "Left" => @Icons.Material.Filled.AlignHorizontalLeft,
+                "Popup" => @Icons.Material.Filled.Window,
+                _ => @Icons.Material.Filled.DeviceUnknown,
+            };
+        }
+    }
+}
---
+++ Shared/Enums/PlacementZone.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Shared.Enums
+{
+    public enum PlacementZone
+    {
+        [Description("Left")]
+        Left = 1,
+        [Description("Middle")]
+        Middle = 2,
+        [Description("Popup")]
+        Popup = 3,
+        [Description("Right")]
+        Right = 4,
+        [Description("Top")]
+        Top = 5,
+    }
+}
